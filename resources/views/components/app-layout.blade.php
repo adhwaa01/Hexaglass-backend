bac0510d15b898a8bdaeb3e20e547ca5592c651f
adhwaa01 <adhwaarajib1@gmail.com>
2026-06-05 00:41:47 +0700

    Fix: Correct Blade component syntax - use extends instead of x-layouts.app

--- resources/views/components/app-layout.blade.php
+++ resources/views/components/app-layout.blade.php
@@ -1,9 +1,5 @@
-@props(['title' => 'Dashboard'])
+@extends('layouts.app', ['title' => $title ?? 'Dashboard'])
 
-@php
-    // Inject title ke layout
-@endphp
-
-<x-layouts.app :title="$title">
+@section('content')
     {{ $slot }}
-</x-layouts.app>
+@endsection
